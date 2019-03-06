e03ae20a7e3dd4945da557bc4d46a3a706ec7cf1
Ajay DSouza <me@ajaydsouza.com>
2019-03-06 23:34:01 +0000

    Multisite support
    Fixes https://github.com/ajaydsouza/autoclose/issues/5

--- uninstall.php
+++ uninstall.php
@@ -5,18 +5,49 @@
  * @package AutoClose
  */
 
-// If uninstall not called from WordPress, then exit
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-if ( wp_next_scheduled( 'acc_cron_hook' ) ) {
-	wp_clear_scheduled_hook( 'acc_cron_hook' );
+global $wpdb;
+
+if ( is_multisite() ) {
+
+	// Get all blogs in the network and activate plugin on each one.
+	$blogids = $wpdb->get_col( //phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching
+		"
+		SELECT blog_id FROM $wpdb->blogs
+		WHERE archived = '0' AND spam = '0' AND deleted = '0'
+	"
+	);
+
+	foreach ( $blogids as $blogid ) {
+		switch_to_blog( $blogid );
+		acc_delete_data();
+		restore_current_blog();
+	}
+} else {
+	acc_delete_data();
 }
 
-if ( wp_next_scheduled( 'ald_acc_hook' ) ) {
-	wp_clear_scheduled_hook( 'ald_acc_hook' );
+
+/**
+ * Delete Data.
+ *
+ * @since 2.0.0
+ */
+function acc_delete_data() {
+
+	delete_option( 'acc_settings' );
+
+	if ( wp_next_scheduled( 'acc_cron_hook' ) ) {
+		wp_clear_scheduled_hook( 'acc_cron_hook' );
+	}
+
+	if ( wp_next_scheduled( 'ald_acc_hook' ) ) {
+		wp_clear_scheduled_hook( 'ald_acc_hook' );
+	}
+
 }
 
-delete_option( 'ald_acc_settings' );
 
