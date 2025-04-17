0c6816c929c097f8a96c5373c65e5a7ae052ed65
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2025-04-17 17:26:14 +0100

    Update bootstrap.php

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -24,7 +24,7 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __DIR__ ) . '/add-to-all.php';
+	require dirname( __DIR__ ) . '/autoclose.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
